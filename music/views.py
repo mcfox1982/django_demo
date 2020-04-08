f3a0df5f6ba503616c701dc316408269b078a8b7
mcfox <mcfox2010@gmail.com>
2020-04-08 16:32:47 +0800

    change

--- music/views.py
+++ music/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse('<h1>This is music site</h1>')
+
+def detail(request, album_id):
+    return HttpResponse('<h2>Detail for album ID:' + str(album_id) + '</h2>')
