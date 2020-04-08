91ec296403dd2bea8663425ee7e6348140b87e18
Jimmy.jin <Jimmy.jin@usedu.club>
2020-04-08 21:38:02 +0800

    change

--- music/views.py
+++ music/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import *
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>This is music site</h1>')
+    all_album = Album.objects.all()
+    html = ''
+    for album in all_album:
+        url = '/music/' + str(album.id) + '/'
+        html += '<a href="' + url + '">' + album.album_title + '</a><br>'
+    return HttpResponse(html)
 
 def detail(request, album_id):
     return HttpResponse('<h2>Detail for album ID:' + str(album_id) + '</h2>')
