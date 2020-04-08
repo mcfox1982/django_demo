5baa9f9572353a063eb06210ec3771dee82e7e53
Jimmy.jin <Jimmy.jin@usedu.club>
2020-04-08 21:49:59 +0800

    change

--- music/views.py
+++ music/views.py
@@ -7,11 +7,11 @@ from .models import *
 # Create your views here.
 def index(request):
     all_album = Album.objects.all()
-    html = ''
-    for album in all_album:
-        url = '/music/' + str(album.id) + '/'
-        html += '<a href="' + url + '">' + album.album_title + '</a><br>'
-    return HttpResponse(html)
+    template = loader.get_template('index.html')
+    context ={
+        'all_album' : all_album ,
+    }
+    return render(request, 'index.html', context)
 
 def detail(request, album_id):
     return HttpResponse('<h2>Detail for album ID:' + str(album_id) + '</h2>')
